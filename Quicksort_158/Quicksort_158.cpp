7809e0c1a36e353c440af68810d97f61105204cf
iqbaldf7 <iqbalfathonah10@gmail.com>
2023-03-31 11:06:41 +0700

    untuk void

--- Quicksort_158/Quicksort_158.cpp
+++ Quicksort_158/Quicksort_158.cpp
@@ -5,3 +5,16 @@ int arr[20];
 int cpm_count = 0;
 int mov_count = 0;
 int n;
+
+void input () {
+	while (true) {
+		cout << "masukan panjang element array" << endl;
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "/masukan panjang array adalah 20" << endl;
+	}
+
+	cout << 
